9337647eae4017be113d2ccd4e2fb23d697c7db2
SurajKumar287 <surajkumar734731@gmail.com>
2025-05-14 10:24:05 +0530

    deleting files

--- CollegeAdmissionForm/src/main/java/com/CollageAdmissionForm/service/AdmissionService.java
+++
@@ -1,9 +0,0 @@
-package com.CollageAdmissionForm.service;
-
-import com.CollageAdmissionForm.model.RegistrationDetails;
-
-public interface AdmissionService {
-
-	void saveData(RegistrationDetails details);
-
-}
